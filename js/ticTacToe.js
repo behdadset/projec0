ac14eda795e371913d31e913ceec54f13be6fef6
behdad setoodegan <behdadsetoodegan@gmail.com>
2020-06-01 14:20:55 +1000

    start

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -1,20 +1,5 @@
 
-const cypher = function(msg){
-  let code = "";
-  for (let i = 0; i < msg.length; i++) {
-    const elementCode = msg.charCodeAt(i);
-    //console.log(elementCode)
-    for (let y = 1; y <= 6; y++){
-      for (let x = 1; x <= 5; x++){
-        if(elementCode === (y*5+x+91)){
-          console.log(x + "" + y)
-        }
-
-      }
-    }
-  
-  }
-  
+const myFunction = function(){
+  $(`.one`).removeAttr('src');
+  $(`.one`).attr('src', 'images/O.jpg')
 }
-
-console.log(cypher("ciphers are confusing!"));
