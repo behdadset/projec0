4d43e5077f4a615a8844b9f89c00f5511f19d247
behdad setoodegan <behdadsetoodegan@gmail.com>
2020-06-01 18:33:32 +1000

    css

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -1,5 +1,14 @@
-
-const myFunction = function(){
-  $(`.one`).removeAttr('src');
-  $(`.one`).attr('src', 'images/O.jpg')
+let turn = 0;
+const myFunction = function(box){
+  console.log(`box ${box}`);
+  if (turn === 0){
+    $(`.${box}`).attr('id', 'x');
+    turn = 1;
+  } else {
+    $(`.${box}`).attr('id', 'o');
+    turn = 0;
+  }
+  
+  //$(`.box ${box}`).css('background-image', 'url( "images/o.png" )');
 }
+
